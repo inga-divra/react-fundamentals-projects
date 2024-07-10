ab7a4ebfa18b56630acd1f75f3c86de28a0131ca
inga-divra <ingady@gmail.com>
2024-07-10 23:24:42 +0300

    Added 05-sidebar-modal project to the repository

---
+++ 05-sidebar-modal/src/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+import { FaBars } from 'react-icons/fa'
+
+const Home = () => {
+    const { openSidebar, openModal } = useGlobalContext()
+    console.log(openSidebar);
+    return (
+        <main>
+            <button
+                className="sidebar-toggle"
+                onClick={openSidebar}>
+                <FaBars />
+            </button>
+            <button
+                className="btn"
+                onClick={openModal}>
+                show modal
+            </button>
+        </main>
+    )
+}
+
+export default Home
